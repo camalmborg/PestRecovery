0aa1c02d13f064e5cafe94522ddec9af604d0603
camalmborg <malmborg@bu.edu>
2024-12-19 18:16:11 -0500

    oak tree data prep

--- SM_HF_Field_Data_Cleaning_Script.R
+++ SM_HF_Field_Data_Cleaning_Script.R
@@ -26,13 +26,40 @@ plots <- plots %>%
 ## Trees data:
 BAF = 10  # basal area factor from variable radius plots
 trees <- trees %>%
-  mutate(plot = str_replace(plot, " ", "-")) %>% 
-  mutate(hotspot = as.numeric(substr(plot, 1, 1))) %>%
-  mutate(ba = (dbh^2) * 0.005454) %>% # basal area calculation from dbh  #%>%
+  mutate(plot = str_replace(plot, " ", "-")) %>%  # clean plot names
+  mutate(hotspot = as.numeric(substr(plot, 1, 1))) %>%  # add hotspot column
+  mutate(ba = (dbh^2) * 0.005454) %>% # basal area calculation from dbh
   group_by(hotspot,plot) %>% mutate(treeper  = n()) %>%  # number of trees per plot
   group_by(hotspot) %>% mutate(hotcount = length(unique(plot))) %>%  # number of plots per hotspot
+  group_by(plot) %>% mutate(tba = sum(ba)) %>%
   mutate(exp_fac = BAF / ba / hotcount) %>% # expansion factor, BAF/BA/number in plot
   group_by(hotspot) %>% mutate(TPA = sum(exp_fac))
+
+## Oak-specific data:
+oaks <- c("BO", "RO", "WO") # oak species for subsetting
+
+percent <- function(x, count){  # percentage function for calculating ba and dead tree percentages
+  (x / count)*100
+}
+
+oaktrees <- subset(trees, spp %in% oaks)
+oaktrees <- oaktrees %>% 
+  group_by(plot) %>% mutate(tot_oak = n(),  # number of oaks
+                            dead = ifelse(Cond == "L", 0, 1),
+                            tba_oak = sum(ba),  # total pak basal area per plot
+                            pba = percent(tba_oak,tba),  # percent oak basal area in plot
+                            deadoak = length(which(Cond == "D")),  # total dead oaks
+                            pdead = percent(deadoak,treeper),  # percent dead oak of all trees in plot
+                            pdo = percent(deadoak,tot_oak)) %>%  # percent dead oak of all oaks in plot
+  group_by(plot,Cond) %>% mutate(dba = sum(ba)*dead,
+                                 pdba = percent(dba,tba)) %>%
+  group_by(hotspot) %>% mutate(tba_oak_hot = sum(ba),  # total oak basal area in hotspot
+                               pba_hot = percent(tba_oak,tba),  # percent oak basal area in hotspot
+                               deadoak_hot = sum(deadoak),  # sum dead oak in hotspot
+                               pdo_hot = percent(deadoak,tot_oak),  # percent dead oak in hotspot
+                               dba_hot = sum(dba),  # total dead basal area in hot spot
+                               pdba_hot = percent(dba_hot,sum(tba))) # percent dead basal area in hotspot
+                            
 
 # Update timeline:
 # 2022-12-19
