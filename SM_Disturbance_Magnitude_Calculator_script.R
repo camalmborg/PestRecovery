244c2a0c75bdcb49ffc53da5cec787c58c51fdb1
Charlotte Malmborg <charlottemalmborg@Charlottes-MacBook-Pro.local>
2025-02-20 16:27:23 -0500

    first commit for disturbance magnitude script

---
+++ SM_Disturbance_Magnitude_Calculator_script.R
@@ -0,0 +1,12 @@
+### This is the script for Harvard Forest 2022 field site disturbance magnitude and disturbance occurrance
+### The script calculates a disturbance magnitude for the years of a disturbance, in this case 2016-2017 (into 2018)
+
+#### ----- Libraries ----- ####
+#install.packages("librarian")
+librarian::shelf(tidyverse, dplyr, googledrive)
+
+#### ----- Load and set up data ----- ####
+
+
+# Update timeline
+# 2025-02-20 created script
