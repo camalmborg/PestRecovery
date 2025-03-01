2ad329905084f3b2265ad907797ce852dcf12fa7
Charlotte Malmborg <charlottemalmborg@Charlottes-MacBook-Pro.local>
2025-03-01 10:52:50 -0500

    update to dist mag calculator - adding site column

--- 04_SM_Disturbance_Magnitude_Calculator_script.R
+++ 04_SM_Disturbance_Magnitude_Calculator_script.R
@@ -16,6 +16,7 @@ librarian::shelf(tidyverse, dplyr, googledrive)
 dist_mag_calc <- function(ts, distyr, scrs){
   # separate columns with just the canopy observation data:
   series <- ts[,grep("^2", names(ts))]
+  series$site <- 1:nrow(series)
   # get disturbance onset column:
   dist <- grep(as.character(distyr), colnames(series))
   # calculate a pre-disturbance steady state to compare disturbance condition:
@@ -23,19 +24,21 @@ dist_mag_calc <- function(ts, distyr, scrs){
   # calculate disturbance magnitudes:
   dmagy1 <- steady - series[,dist]      # onset year
   dmagy2 <- steady - series[,dist+1]    # following year
-  # calculating disturbance thresholds:
-  # sc_series <- scrs[,grep("^2", names(scrs))]  # use condition scores to identify sites @ threshold
-  # d <- quantile(sc_series[,(dist-6):(dist-1)], c(0.01), na.rm = T)  # 1% quantile in 5yr pre-onset
-  # d1 <- scrs[,dist] < d
-  # d2 <- scrs[,dist+1] < d
   # disturbance magnitudes being > 0 is defol:
   def1 <- dmagy1 > 0
   def2 <- dmagy2 > 0
   # combine dataset for output
-  dmag_data <- cbind(series[,dist], series[,dist+1], steady, dmagy1, dmagy2, def1, def2)
-  colnames(dmag_data) <- c(names(series[dist]), names(series[dist+1]), "steady", "dmag1", "dmag2", "dm_gt_0_y1", "dm_gt_0_y2")
+  dmag_data <- cbind(series[,'site'], series[,dist], series[,dist+1], steady, dmagy1, dmagy2, def1, def2)
+  colnames(dmag_data) <- c("site", names(series[dist]), names(series[dist+1]), "steady", "dmag1", "dmag2", "dm_gt_0_y1", "dm_gt_0_y2")
   return(dmag_data)
 }
 
 # Update timeline
 # 2025-02-20 created script
+
+####----ARCHIVE----####
+# # calculating disturbance thresholds:
+# sc_series <- scrs[,grep("^2", names(scrs))]  # use condition scores to identify sites @ threshold
+# d <- quantile(sc_series[,(dist-6):(dist-1)], c(0.05), na.rm = T)  # 5% quantile in 5yr pre-onset
+# d1 <- scrs[,dist] < d
+# d2 <- scrs[,dist+1] < d
