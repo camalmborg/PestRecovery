d06fbb5394180546df44cd019fcb4a3b596859f7
Charlotte Malmborg <charlottemalmborg@crc-dot1x-nat-10-239-180-58.bu.edu>
2025-03-12 17:08:22 -0400

    setting up data for mortality analyses

--- 06_Mortality_Analyses_script.R
+++ 06_Mortality_Analyses_script.R
@@ -11,12 +11,30 @@ librarian::shelf(tidyverse, dplyr, ggplot2, mgcv)
 # harvard forest plot data
 
 #### ----- prepping data to run models ----- ####
-# preparing plot-level information from tree data:
+## preparing plot-level information from tree data:
 tree_to_plot <- trees %>%
   select(hotspot, point, plot, tot_tree, tot_dead, pdead, tot_dba, pdba) %>%
-  group_by(plot) %>% summarise(across(everything(), first, .groups = "keep"))
+  group_by(plot) %>% summarise(across(everything(), first))
 
 notree <- setdiff(plots$plot, tree_to_plot$plot)
+
+## making the matrix for predictors variables:
+# disturbance magnitude and tcg/scores during disturbance:
+cols <- c(grep("^20", names(hf_dmag)), 
+          grep("^dmag", names(hf_dmag)))
+dmags <- cbind(hf_dmag[cols], hf_dmag_cs[cols])[-which(plots$plot == notree),]  # remote no-tree plot
+colnames(dmags) <- c("tcg_y1", "tcg_y2", "dmag_y1", "dmag_y2",
+                     "cs_y1", "cs_y2", "dmag_cs_y1", "dmag_cs_y2")
+
+## daymet data:
+# making seasonal values:
+spring = c(3,4,5)
+summer = c(6,7,8)
+seas_daym <- hf_daym %>%
+  group_by(site, year) %>% filter(month %in% spring) #%>%
+  group_by(year) %>% aggregate()
+
+# combine data:
 
 #### ----- running models ----- ####
 # using beta regression in mgcv package
