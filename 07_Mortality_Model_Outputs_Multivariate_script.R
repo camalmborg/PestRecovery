b8efa34df634f585f5ce3d3045fe1b7edb12dabd
Charlotte Malmborg <malmborg@scc-dd3.scc.bu.edu>
2025-04-29 07:16:51 -0400

    first commit for multivariate mortality runs output script

---
+++ 07_Mortality_Model_Outputs_Multivariate_script.R
@@ -0,0 +1,70 @@
+# Script for visualizing, parsing, and plotting model outputs from multivariate mortality models
+
+# load libraries
+librarian::shelf(tidyverse, dplyr, rjags, coda)
+
+# navigate to folder:
+dir <- "/projectnb/dietzelab/malmborg/Ch2_PestRecovery/Mortality_Model_Runs/"
+setwd(dir)
+
+### Functions to get model outputs
+# get model output:
+#'@param dir = folder where the model_outputs and model_runs are located
+#'@param modelnum = model number for multivariate run
+mort_out <- function(dir, modelnum){
+  num <- modelnum
+  file <- list.files(paste0(dir, "model_outputs/"))
+  multis <- file[grep("multi", file)]
+  name <-  grep(paste0("modelrun_", as.character(modelnum), "_"), multis)
+  model <- read.csv(paste0(dir, "model_outputs/", multis[name]))
+  return(model)
+}
+
+# get model result object:
+mort_data <- function(dir, modelnum){
+  load(paste0(dir, "model_runs/", file[name]))
+  num <- modelnum
+  file <- list.files(paste0(dir, "model_runs/"))
+  multis <- file[grep("covs", file)]
+  name <-  grep(paste0("modelrun_", as.character(modelnum), "_"), multis)
+  load(paste0(dir, "model_runs/", file[name]))
+  return(model_info)
+}
+
+mort_data_load <- function(dir, model){
+  load(paste0(dir, "model_runs/", model))
+  return(model_info)
+}
+
+### loop for recording multivariate dics
+# model_list:
+models <- list.files(paste0(dir, "model_runs"))
+model_list <- models[grep("covs", models)]
+# make empty list for filling in dics:
+mort_dics <- matrix(NA, nrow = length(model_list), ncol = 5)
+#colnames(mort_dics) <- c("id", "model", "DIC")
+colnames(mort_dics) <- c("id", "model", "ncovs" ,"covars", "DIC")
+# the loop:
+for (i in 1:length(model_list)){
+  # selecting model:
+  dir = dir
+  # load model and output:
+  model <- mort_data_load(dir, model_list[i])
+  # collect dic and model identity:
+  mort_dics[i, 1] <- i
+  mort_dics[i, 2] <- model$metadata$run
+  # number of covariates:
+  mort_dics[i, 3] <- model$metadata$ncovs
+  # covariates included (column numbers of multivariate preds object):
+  mort_dics[i, 4] <- paste(model$metadata$covars, collapse = " ")
+  # model dic:
+  mort_dics[i, 5] <- model$dic[[2]]
+  # tracking loop repeats:
+  print(i)
+}
+mort_dic <- as.data.frame(mort_dics)
+mort_dic$delDIC <- as.numeric(mort_dic$DIC) - min(as.numeric(mort_dic$DIC))
+mort_dic <- mort_dic[order(mort_dic$delDIC),]
+mort_dic$perform <- 1:nrow(mort_dic)
+#write.csv(mort_dic, file = "2025_04_29_multi_mortality_models_dic_table.csv")
+
