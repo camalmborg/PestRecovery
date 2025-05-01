b162b1ae67cd85053eb24fb97dca0ec0d250a5aa
Charlotte Malmborg <malmborg@scc-gl1.scc.bu.edu>
2025-05-01 14:19:32 -0400

    first commit - figures script

---
+++ 08_Mortality_Analyses_Figures_Script.R
@@ -0,0 +1,8 @@
+# Script for making figures for mortality analyses
+
+# load libraries
+librarian::shelf(tidyverse, dplyr, ggplot2, RColorBrewer)
+
+# # navigate to folder:
+# dir <- "/projectnb/dietzelab/malmborg/Ch2_PestRecovery/"
+# setwd(dir)
