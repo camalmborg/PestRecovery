dba289bad0a4e00ddcade8e19b9997a8e4a4c801
Charlotte Malmborg <malmborg@scc-be1.scc.bu.edu>
2025-05-08 17:00:31 -0400

    started making ggplot for EFI figures

--- 08_Mortality_Analyses_Figures_Script.R
+++ 08_Mortality_Analyses_Figures_Script.R
@@ -1,9 +1,25 @@
 # Script for making figures for mortality analyses
 
 # load libraries
-librarian::shelf(tidyverse, dplyr, ggplot2, RColorBrewer)
+#install.packages("ggplot2")
+#install.packages("hrbrthemes")
+librarian::shelf(tidyverse, dplyr, ggplot2, RColorBrewer, hrbrthemes)
 
 # # navigate to folder:
 # dir <- "/projectnb/dietzelab/malmborg/Ch2_PestRecovery/"
 # setwd(dir)
 
+## plotting percent dead basal area observed and predicted
+# make the data set:
+plot_data <- data.frame(
+  x = 1:nrow(data_sort),
+  y_obs = data_sort$pdba,
+  y_pred = y,                         # predicted in 07_Mortality_Model_Outputs_Results_script, 5/8/25 provisional
+  hot = data_sort$hotspot
+)
+# making plot:
+pdba_pred_obs <- ggplot(plot_data, aes(x = x, y = y_obs)) +
+  geom_point() +
+  geom_point(aes(x = x, y = y_pred)
+)
+pdba_pred_obs
