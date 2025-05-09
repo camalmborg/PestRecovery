ea899d839ee9f7e51316c48228eed625b3a88634
Charlotte Malmborg <malmborg@scc-be6.scc.bu.edu>
2025-05-09 14:33:59 -0400

    plots updates

--- 08_Mortality_Analyses_Figures_Script.R
+++ 08_Mortality_Analyses_Figures_Script.R
@@ -14,12 +14,18 @@ librarian::shelf(tidyverse, dplyr, ggplot2, RColorBrewer, hrbrthemes)
 plot_data <- data.frame(
   x = 1:nrow(data_sort),
   y_obs = data_sort$pdba,
-  y_pred = y,                         # predicted in 07_Mortality_Model_Outputs_Results_script, 5/8/25 provisional
+  y_pred = apply(ypred, 2, mean),
   hot = data_sort$hotspot
 )
 # making plot:
 pdba_pred_obs <- ggplot(plot_data, aes(x = x, y = y_obs)) +
   geom_point() +
-  geom_point(aes(x = x, y = y_pred)
-)
+  geom_point(aes(x = x, y = y_pred),
+             col = "red") +
+  geom_line(aes(x = x, y = ci[1,])) +
+  geom_line(aes(x = x, y = ci[3,]))
 pdba_pred_obs
+
+pred_obs_compare <- ggplot(plot_data, aes(x = y_pred, y = y_obs)) +
+  geom_point()
+pred_obs_compare
