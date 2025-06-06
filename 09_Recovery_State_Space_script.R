60a57b75f17ddbb30100a0ddd3e014f2090bfe1b
Charlotte Malmborg <malmborg@scc-gl1.scc.bu.edu>
2025-06-06 14:29:51 -0400

    adding site and year effect loops in jags model

--- 09_Recovery_State_Space_script.R
+++ 09_Recovery_State_Space_script.R
@@ -21,16 +21,27 @@ for (s in sites){
 
 ### Process Model:
 for (t in 2:n){
-    R[t,s] <- r0 #+ betar[z]  # here is where covariates of R would go?
-    mu[t,s] <- R[t,s] * x[t-1,s]
+    R[t,s] <- r0 #+ betar[z]  # here is where covariates of R would go? + alpha site + alpha t
+    mu[t,s] <- R[t,s] * x[t-1,s]  # logit here?
     x[t,s] ~ dnorm(mu[t,s], tau_add)
   }
   
 }
+
+### Random Effects: (add later)
+# for (s in sites){
+#   asite ~ dnorm(0, tausite)
+# }
+# 
+# for (t in 1:n){
+#   atime ~ dnorm(0, tautime)
+# }
 
 ### Priors:
 x[1] ~ dnorm(x_ic, t_ic)
 r0 ~ dnorm(r_ic, rprec)  # initial condition r
 tau_obs ~ dgamma(t_obs, a_obs)
 tau_add ~ dgamma(t_add, a_add)
+#tausite ~ dgamma(0.001, 0.001)
+#tautime ~ dgamma(0.001, 0.001)
 "
