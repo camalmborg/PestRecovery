12166ce9c945fbb9f3c1af2cfc100113c55ada6a
Charlotte Malmborg <malmborg@scc-cb8.scc.bu.edu>
2025-06-07 16:08:10 -0400

    updated model to run across sites and time, initializes

--- 09_Recovery_State_Space_script.R
+++ 09_Recovery_State_Space_script.R
@@ -6,26 +6,30 @@ librarian::shelf(rjags, coda)
 
 # time series data:
 tcg <- tcg_ts  # harvard forest time series data from making plots for hotspot means - will need to update code for gee data
-# time series length:
-time = 1:ncol(tcg)
-nt = length(time)
 
-recov_state_space <- "
-model {
+# make the disturbance magnitude version:
+start <- grep("^2013", names(tcg))
+end <- grep("^2015", names(tcg))
+steady_state <- apply(tcg[,start:end], 1, mean)
+dist <- grep("^2017", names(tcg))
+post_dist <- tcg[,(dist + 1):ncol(tcg)]
+dm_post_dist <- steady_state - post_dist
+
+recov_state_space <- "model {
 for (s in sites){
 
 ### Data Model:
-  for (t in 1:n){
-    y[t,s] ~ dnorm(x[t,s], tau_obs
+  for (t in 1:nt){
+    y[s,t] ~ dnorm(x[s,t], tau_obs)
   }
 
 ### Process Model:
-for (t in 2:n){
-    R[t,s] <- r0 #+ betar[z]  # here is where covariates of R would go? + alpha site + alpha t
-    mu[t,s] <- R[t,s] * x[t-1,s]  # logit here?
-    x[t,s] ~ dnorm(mu[t,s], tau_add)
+for (t in 2:nt){
+    R[s,t] <- r0
+    mu[s,t] <- R[s,t] * x[s,t-1]  
+    x[s,t] ~ dnorm(mu[s,t], tau_add)
   }
-  
+  x[s,1] ~ dnorm(x_ic, t_ic)
 }
 
 ### Random Effects: (add later)
@@ -38,10 +42,33 @@ for (t in 2:n){
 # }
 
 ### Priors:
-x[1] ~ dnorm(x_ic, t_ic)
-r0 ~ dnorm(r_ic, rprec)  # initial condition r
+r0 ~ dnorm(r_ic, r_prec)  # initial condition r
 tau_obs ~ dgamma(t_obs, a_obs)
 tau_add ~ dgamma(t_add, a_add)
 #tausite ~ dgamma(0.001, 0.001)
 #tautime ~ dgamma(0.001, 0.001)
+}
 "
+
+# data for model:
+recov_sample <- as.matrix(dm_post_dist[sort(sample(1:nrow(tcg), 10)),])
+# time series length:
+time = 1:ncol(recov_sample)
+sites = 1:nrow(recov_sample)
+# first x:
+x1 <- mean(tcg[,grep("^2017",names(tcg))])
+
+# make list object
+model_data <- list(y = recov_sample,
+                   nt = length(time),
+                   sites = sites, 
+                   t_obs = 0.001, a_obs = 0.001,
+                   t_add = 0.001, a_add = 0.001,
+                   r_ic = 1, r_prec = 0.001,
+                   x_ic = x1, t_ic = 0.01)
+
+# attempt model run:
+jags_model <- jags.model(file = textConnection(recov_state_space),
+                         data = model_data, 
+                         n.chains = 3)
+
