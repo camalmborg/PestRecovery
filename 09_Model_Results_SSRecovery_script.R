d1b959e5a3fc803c2ca3037f4dfd1853195296c5
Charlotte Malmborg <malmborg@scc-uq4.scc.bu.edu>
2025-06-25 13:06:11 -0400

    first commit checking out state space models

---
+++ 09_Model_Results_SSRecovery_script.R
@@ -0,0 +1,8 @@
+# Checkin' out the recovery state space models
+
+## Set up
+# load libraries:
+librarian::shelf(rjags, coda, dplyr)
+
+# set correct working directory
+setwd("/projectnb/dietzelab/malmborg/Ch2_PestRecovery/Recovery_State_Space_Runs/")
