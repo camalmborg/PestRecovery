46020f3e5ef9a058482b57b0048a4a0fc66708b0
Charlotte Malmborg <malmborg@scc-gb3.scc.bu.edu>
2025-07-23 17:07:45 -0400

    using the plotting for test runs of time-varying model runs

--- 09_Recovery_State_Space_Outputs_Results_script.R
+++ 09_Recovery_State_Space_Outputs_Results_script.R
@@ -17,20 +17,20 @@ models <- list.files(paste0(dir, "model_runs"))[grep("RData", list.files(paste0(
 # 
 # # load jags output:
 # jags_out <- model_info$jags_out
-# vars <- varnames(jags_out)
-# params <- jags_out[,grep("r0|^tau", vars)]
-# R_samp <- sample(vars[grep("R", vars)], 6)
-# x_samp <- sample(vars[grep("x", vars)], 6)
-# R_params <- jags_out[,R_samp]
-# x_params <- jags_out[,x_samp]
-# 
-# # for the random effects model
-# atime_params <- jags_out[,grep("^at", vars)]
-# asite_samp <- sample(vars[grep("^as", vars)], 6)
-# asite_params <- jags_out[,asite_samp]
-# 
-# # for betas (univariate):
-# beta_params <- jags_out[,grep("^b", vars)]
+vars <- varnames(jags_out)
+params <- jags_out[,grep("r0|^tau", vars)]
+R_samp <- sample(vars[grep("R", vars)], 6)
+x_samp <- sample(vars[grep("x", vars)], 6)
+R_params <- jags_out[,R_samp]
+x_params <- jags_out[,x_samp]
+
+# for the random effects model
+atime_params <- jags_out[,grep("^at", vars)]
+asite_samp <- sample(vars[grep("^as", vars)], 6)
+asite_params <- jags_out[,asite_samp]
+
+# for betas (univariate):
+beta_params <- jags_out[,grep("^b", vars)]
 
 
 
