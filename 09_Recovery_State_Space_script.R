019d0495b318f3f59301daf56ea458b7535dde2d
Charlotte Malmborg <malmborg@scc-be7.scc.bu.edu>
2025-07-24 08:37:45 -0400

    prep for runs with just time or just space random effects (not both) and setting to run on SCC

--- 09_Recovery_State_Space_script.R
+++ 09_Recovery_State_Space_script.R
@@ -39,75 +39,156 @@ model_data <- list(y = recov_data,
                    x_ic = x1, t_ic = 0.01)
 
 
-### "Base Model" - no covariates or random effects:
-recov_state_space <- "model {
-for (s in sites){
+# ### "Base Model" - no covariates or random effects:
+# recov_state_space <- "model {
+# for (s in sites){
+# 
+# ### Data Model:
+#   for (t in 1:nt){
+#     y[s,t] ~ dnorm(x[s,t], tau_obs)
+#   }
+# 
+# ### Process Model:
+# for (t in 2:nt){
+#     R[s,t] <- r0
+#     mu[s,t] <- R[s,t] * x[s,t-1]
+#     x[s,t] ~ dnorm(mu[s,t], tau_add)
+#   }
+#   x[s,1] ~ dnorm(x_ic, t_ic)
+# }
+# 
+# ### Priors:
+# r0 ~ dnorm(r_ic, r_prec)  # initial condition r
+# tau_obs ~ dgamma(t_obs, a_obs)
+# tau_add ~ dgamma(t_add, a_add)
+# }
+# "
+# 
+# # base model run:
+# jags_model <- jags.model(file = textConnection(recov_state_space),
+#                          data = model_data,
+#                          n.chains = 3)
+# jags_out <- coda.samples(jags_model,
+#                           variable.names = c("x", "R",
+#                                              "tau_obs", "tau_add",
+#                                              "r0"),
+#                           n.iter = 150000,
+#                           adapt = 50000,
+#                           thin = 50)
+# 
+# # run DIC
+# DIC <- dic.samples(jags_model, n.iter = 50000)
+# sum <- sum(DIC$deviance, DIC$penalty)
+# 
+# # Make output list
+# # track metadata:
+# metadata <- tibble::lst(recov_state_space, model_data)
+# # model selection
+# dic <- list(DIC, sum)
+# # model output
+# out <- as.matrix(jags_out)
+# # combine output
+# model_output <- tibble::lst(metadata, dic, jags_out, out)
+# 
+# # save base model output:
+# out_path <- "/projectnb/dietzelab/malmborg/Ch2_PestRecovery/Recovery_State_Space_Runs/model_outputs/"
+# run_path <- "/projectnb/dietzelab/malmborg/Ch2_PestRecovery/Recovery_State_Space_Runs/model_runs/"
+# date <- as.character(Sys.Date())
+# filename_outputs <- paste0(out_path, date, "_base_model","_output.csv")
+# filename_runs <- paste0(run_path, date, "_base_model", "_data.RData")
+# 
+# # save output
+# write.csv(out, file = filename_outputs)
+# 
+# # save model selection and metadata to folder
+# model_info <- model_output[c('jags_out', 'dic', 'metadata')]
+# save(model_info, file = filename_runs)
+# 
+# 
+# 
+# ### "Random Effects Model: option 2" - no covariates but includes random effects for site (pixel) and time (year)
+# recov_state_space_re_opt2 <- "model {
+# for (s in sites){
+# 
+# ### Data Model:
+#   for (t in 1:nt){
+#     y[s,t] ~ dnorm(x[s,t], tau_obs)
+#   }
+# 
+# ### Process Model:
+# for (t in 2:nt){
+#     R[s,t] <- r0 + asite[s] + atime[t-1]  
+#     mu[s,t] <- R[s,t] * x[s,t-1]  
+#     x[s,t] ~ dnorm(mu[s,t], tau_add)
+#   }
+#   x[s,1] ~ dnorm(x_ic, t_ic)
+# }
+# 
+# ### Random Effects: 
+# for (s in sites){
+#   asite[s] ~ dnorm(0, tausite)
+# }
+# 
+# 
+# atime[1] = 0                   # option 2: indexing for atime[0]
+# for (t in 2:(nt-1)){
+#   atime[t] ~ dnorm(0, tautime)
+# }
+# 
+# ### Priors:
+# r0 ~ dnorm(r_ic, r_prec)  # initial condition r
+# tau_obs ~ dgamma(t_obs, a_obs)
+# tau_add ~ dgamma(t_add, a_add)
+# tausite ~ dgamma(0.001, 0.001)
+# tautime ~ dgamma(0.001, 0.001)
+# }
+# "
+# 
+# # base RE model run:
+# jags_model <- jags.model(file = textConnection(recov_state_space_re_opt2),
+#                          data = model_data, 
+#                          n.chains = 3)
+# jags_out <- coda.samples(jags_model,
+#                          variable.names = c("atime", "asite", "x", "R",
+#                                             "tau_obs", "tau_add",
+#                                             "tausite", "tautime",
+#                                             "r0"),
+#                          n.iter = 150000,
+#                          n.adapt = 50000,
+#                          thin = 50)
+# 
+# # run DIC
+# DIC <- dic.samples(jags_model, n.iter = 50000)
+# sum <- sum(DIC$deviance, DIC$penalty)
+# 
+# # Make output list
+# # track metadata:
+# metadata <- tibble::lst(recov_state_space_re_opt2, model_data)
+# # model selection
+# dic <- list(DIC, sum)
+# # model output
+# out <- as.matrix(jags_out)
+# # combine output
+# model_output <- tibble::lst(metadata, dic, jags_out, out)
+# 
+# # save base model output:
+# out_path <- "/projectnb/dietzelab/malmborg/Ch2_PestRecovery/Recovery_State_Space_Runs/model_outputs/"
+# run_path <- "/projectnb/dietzelab/malmborg/Ch2_PestRecovery/Recovery_State_Space_Runs/model_runs/"
+# date <- as.character(Sys.Date())
+# filename_outputs <- paste0(out_path, date, "_base_RE_model_opt2", "_output.csv")
+# filename_runs <- paste0(run_path, date, "_base_RE_model_opt2", "_data.RData")
+# 
+# # save output
+# write.csv(out, file = filename_outputs)
+# 
+# # save model selection and metadata to folder
+# model_info <- model_output[c('jags_out', 'dic', 'metadata')]
+# save(model_info, file = filename_runs)
+# 
 
-### Data Model:
-  for (t in 1:nt){
-    y[s,t] ~ dnorm(x[s,t], tau_obs)
-  }
 
-### Process Model:
-for (t in 2:nt){
-    R[s,t] <- r0
-    mu[s,t] <- R[s,t] * x[s,t-1]
-    x[s,t] ~ dnorm(mu[s,t], tau_add)
-  }
-  x[s,1] ~ dnorm(x_ic, t_ic)
-}
-
-### Priors:
-r0 ~ dnorm(r_ic, r_prec)  # initial condition r
-tau_obs ~ dgamma(t_obs, a_obs)
-tau_add ~ dgamma(t_add, a_add)
-}
-"
-
-# base model run:
-jags_model <- jags.model(file = textConnection(recov_state_space),
-                         data = model_data,
-                         n.chains = 3)
-jags_out <- coda.samples(jags_model,
-                          variable.names = c("x", "R",
-                                             "tau_obs", "tau_add",
-                                             "r0"),
-                          n.iter = 150000,
-                          adapt = 50000,
-                          thin = 50)
-
-# run DIC
-DIC <- dic.samples(jags_model, n.iter = 50000)
-sum <- sum(DIC$deviance, DIC$penalty)
-
-# Make output list
-# track metadata:
-metadata <- tibble::lst(recov_state_space, model_data)
-# model selection
-dic <- list(DIC, sum)
-# model output
-out <- as.matrix(jags_out)
-# combine output
-model_output <- tibble::lst(metadata, dic, jags_out, out)
-
-# save base model output:
-out_path <- "/projectnb/dietzelab/malmborg/Ch2_PestRecovery/Recovery_State_Space_Runs/model_outputs/"
-run_path <- "/projectnb/dietzelab/malmborg/Ch2_PestRecovery/Recovery_State_Space_Runs/model_runs/"
-date <- as.character(Sys.Date())
-filename_outputs <- paste0(out_path, date, "_base_model","_output.csv")
-filename_runs <- paste0(run_path, date, "_base_model", "_data.RData")
-
-# save output
-write.csv(out, file = filename_outputs)
-
-# save model selection and metadata to folder
-model_info <- model_output[c('jags_out', 'dic', 'metadata')]
-save(model_info, file = filename_runs)
-
-
-
-### "Random Effects Model: option 2" - no covariates but includes random effects for site (pixel) and time (year)
-recov_state_space_re_opt2 <- "model {
+### "Random Effects Model: JUST TIME" - no covariates but includes random effects for time (year)
+recov_state_space_re_time <- "model {
 for (s in sites){
 
 ### Data Model:
@@ -124,10 +205,10 @@ for (t in 2:nt){
   x[s,1] ~ dnorm(x_ic, t_ic)
 }
 
-### Random Effects: (add later)
-for (s in sites){
-  asite[s] ~ dnorm(0, tausite)
-}
+# ### Random Effects: 
+# for (s in sites){
+#   asite[s] ~ dnorm(0, tausite)
+# }
 
 
 atime[1] = 0                   # option 2: indexing for atime[0]
@@ -139,19 +220,19 @@ for (t in 2:(nt-1)){
 r0 ~ dnorm(r_ic, r_prec)  # initial condition r
 tau_obs ~ dgamma(t_obs, a_obs)
 tau_add ~ dgamma(t_add, a_add)
-tausite ~ dgamma(0.001, 0.001)
+#tausite ~ dgamma(0.001, 0.001)
 tautime ~ dgamma(0.001, 0.001)
 }
 "
 
 # base RE model run:
-jags_model <- jags.model(file = textConnection(recov_state_space_re_opt2),
+jags_model <- jags.model(file = textConnection(recov_state_space_re_time),
                          data = model_data, 
                          n.chains = 3)
 jags_out <- coda.samples(jags_model,
                          variable.names = c("atime", "asite", "x", "R",
                                             "tau_obs", "tau_add",
-                                            "tausite", "tautime",
+                                            "tautime",
                                             "r0"),
                          n.iter = 150000,
                          n.adapt = 50000,
@@ -163,7 +244,7 @@ sum <- sum(DIC$deviance, DIC$penalty)
 
 # Make output list
 # track metadata:
-metadata <- tibble::lst(recov_state_space_re_opt2, model_data)
+metadata <- tibble::lst(recov_state_space_re_time, model_data)
 # model selection
 dic <- list(DIC, sum)
 # model output
@@ -175,8 +256,8 @@ model_output <- tibble::lst(metadata, dic, jags_out, out)
 out_path <- "/projectnb/dietzelab/malmborg/Ch2_PestRecovery/Recovery_State_Space_Runs/model_outputs/"
 run_path <- "/projectnb/dietzelab/malmborg/Ch2_PestRecovery/Recovery_State_Space_Runs/model_runs/"
 date <- as.character(Sys.Date())
-filename_outputs <- paste0(out_path, date, "_base_RE_model_opt2", "_output.csv")
-filename_runs <- paste0(run_path, date, "_base_RE_model_opt2", "_data.RData")
+filename_outputs <- paste0(out_path, date, "_base_RE_time_model", "_output.csv")
+filename_runs <- paste0(run_path, date, "_base_RE_time_model", "_data.RData")
 
 # save output
 write.csv(out, file = filename_outputs)
@@ -186,10 +267,84 @@ model_info <- model_output[c('jags_out', 'dic', 'metadata')]
 save(model_info, file = filename_runs)
 
 
+### "Random Effects Model: SPACE" - no covariates but includes random effects for site (pixel)
+recov_state_space_re_space <- "model {
+for (s in sites){
+
+### Data Model:
+  for (t in 1:nt){
+    y[s,t] ~ dnorm(x[s,t], tau_obs)
+  }
+
+### Process Model:
+for (t in 2:nt){
+    R[s,t] <- r0 + asite[s] + atime[t-1]  
+    mu[s,t] <- R[s,t] * x[s,t-1]  
+    x[s,t] ~ dnorm(mu[s,t], tau_add)
+  }
+  x[s,1] ~ dnorm(x_ic, t_ic)
+}
+
+### Random Effects: 
+for (s in sites){
+  asite[s] ~ dnorm(0, tausite)
+}
 
 
+# atime[1] = 0                   # option 2: indexing for atime[0]
+# for (t in 2:(nt-1)){
+#   atime[t] ~ dnorm(0, tautime)
+# }
 
+### Priors:
+r0 ~ dnorm(r_ic, r_prec)  # initial condition r
+tau_obs ~ dgamma(t_obs, a_obs)
+tau_add ~ dgamma(t_add, a_add)
+tausite ~ dgamma(0.001, 0.001)
+#tautime ~ dgamma(0.001, 0.001)
+}
+"
 
+# base RE model run:
+jags_model <- jags.model(file = textConnection(recov_state_space_re_space),
+                         data = model_data, 
+                         n.chains = 3)
+jags_out <- coda.samples(jags_model,
+                         variable.names = c("atime", "asite", "x", "R",
+                                            "tau_obs", "tau_add",
+                                            "tausite", 
+                                            "r0"),
+                         n.iter = 150000,
+                         n.adapt = 50000,
+                         thin = 50)
+
+# run DIC
+DIC <- dic.samples(jags_model, n.iter = 50000)
+sum <- sum(DIC$deviance, DIC$penalty)
+
+# Make output list
+# track metadata:
+metadata <- tibble::lst(recov_state_space_re_space, model_data)
+# model selection
+dic <- list(DIC, sum)
+# model output
+out <- as.matrix(jags_out)
+# combine output
+model_output <- tibble::lst(metadata, dic, jags_out, out)
+
+# save base model output:
+out_path <- "/projectnb/dietzelab/malmborg/Ch2_PestRecovery/Recovery_State_Space_Runs/model_outputs/"
+run_path <- "/projectnb/dietzelab/malmborg/Ch2_PestRecovery/Recovery_State_Space_Runs/model_runs/"
+date <- as.character(Sys.Date())
+filename_outputs <- paste0(out_path, date, "_base_RE_space_model", "_output.csv")
+filename_runs <- paste0(run_path, date, "_base_RE_space_model", "_data.RData")
+
+# save output
+write.csv(out, file = filename_outputs)
+
+# save model selection and metadata to folder
+model_info <- model_output[c('jags_out', 'dic', 'metadata')]
+save(model_info, file = filename_runs)
 
 ##### Archive ####
 # # data for model:
