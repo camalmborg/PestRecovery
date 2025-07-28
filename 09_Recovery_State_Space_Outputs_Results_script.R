9cb89a13b1b01f6d5ab3e839874b92cdba6993bf
Charlotte Malmborg <malmborg@scc-kd3.scc.bu.edu>
2025-07-28 14:48:23 -0400

    updating code for grabbing parameter means for estimating effect sizes

--- 09_Recovery_State_Space_Outputs_Results_script.R
+++ 09_Recovery_State_Space_Outputs_Results_script.R
@@ -11,35 +11,12 @@ setwd(dir)
 # model files:
 models <- list.files(paste0(dir, "model_runs"))[grep("RData", list.files(paste0(dir, "model_runs")))]
 
-
-# # load model
-load(paste0(dir, "model_runs/", models[24]))
-# 
-# # load jags output:
-jags_out <- model_info$jags_out
-vars <- varnames(jags_out)
-params <- jags_out[,grep("r0|^tau", vars)]
-R_samp <- sample(vars[grep("R", vars)], 6)
-x_samp <- sample(vars[grep("x", vars)], 6)
-R_params <- jags_out[,R_samp]
-x_params <- jags_out[,x_samp]
-
-# for the random effects model
-atime_params <- jags_out[,grep("^at", vars)]
-asite_samp <- sample(vars[grep("^as", vars)], 6)
-asite_params <- jags_out[,asite_samp]
-
-# for betas (univariate):
-beta_params <- jags_out[,grep("^b", vars)]
-
-
-
 ### Collecting DICs
 # extract from model metadata:
 model_dics <- matrix(NA, nrow = length(models), ncol = 3)
 colnames(model_dics) <- c("model_number", "covariate", "dic")
 for (i in 1:length(models)){
-  # load model
+  # load model:
   load(paste0(dir, "model_runs/", models[i]))
   # model name:
   name <- models[i]
@@ -62,4 +39,75 @@ dic_sort <- as.data.frame(model_dics[order(as.numeric(model_dics[,"dic"])),], de
 dic_sort$del_dic <- as.numeric(dic_sort$dic) - min(as.numeric(dic_sort$dic)) 
 # save:
 #write.csv(dic_sort, "2025_07_23_uni_static_and_base_recov_models_dics.csv")
-                               
+
+
+### Getting beta parameters and calculating CIs
+# # testing...
+models <- list.files(paste0(dir, "model_runs", "/archive"))#[grep("RData", list.files(paste0(dir, "model_runs")))]
+load(paste0(dir, "model_runs/archive/", models[8]))
+
+#varnames(params)
+model_params <- as.data.frame(matrix(NA, nrow = length(models), ncol = 12))
+model_outputs <- list()
+colnames(model_params) <- c("model_number", "covariate", 
+                            "atime[1]", "atime[2]", "atime[3]", "atime[4]", "atime[5]", "atime[6]",
+                            "beta", "r0", "tau_add", "tau_obs")
+for (i in 1:length(models)){
+  # load model:
+  # load model:
+  load(paste0(dir, "model_runs/", models[i]))
+  # model name:
+  name <- models[i]
+  if (grepl("cov", name) == TRUE){
+    model <- str_match(name, "cov_(.*?)_data")[,2]
+  } else {
+    model <- str_match(name, "_(.*?)_model")[,2]
+  }
+  # load model number and name:
+  model_params[i,1] <- i
+  model_params[i,2] <- model
+  
+  # collect model params:
+  jags_out <- model_info$jags_out
+  vars <- varnames(jags_out)
+  params <- jags_out[,grep("r0|^tau|^b|^at", vars)]
+  # remove burn in:
+  burn_in = 25000
+  params_burn <- window(params, start = burn_in)
+  # convert output to dataframe:
+  params_out <- as.data.frame(as.matrix(params_burn))
+  # save model params to list:
+  model_outputs[[i]] <- params_out
+  # get param means:
+  params_mean <- apply(params_out, 2, mean)
+  # fill in table of parameter means:
+  for (j in varnames(params)){
+    model_params[i,j] <- round(params_mean[j], 3)
+  }
+}
+
+
+### For model convergence checks and tests:
+# # load model
+load(paste0(dir, "model_runs/", models[1]))
+#
+# # load jags output:
+jags_out <- model_info$jags_out
+vars <- varnames(jags_out)
+params <- jags_out[,grep("r0|^tau", vars)]
+R_samp <- sample(vars[grep("R", vars)], 6)
+x_samp <- sample(vars[grep("x", vars)], 6)
+R_params <- jags_out[,R_samp]
+x_params <- jags_out[,x_samp]
+
+# for the random effects model
+atime_params <- jags_out[,grep("^at", vars)]
+# asite_samp <- sample(vars[grep("^as", vars)], 6)
+# asite_params <- jags_out[,asite_samp]
+
+# for betas (univariate):
+beta_params <- jags_out[,grep("^b", vars)]
+
+# testing convergence with plots
+# testing effective sizes
+# gelman to determine burn in
