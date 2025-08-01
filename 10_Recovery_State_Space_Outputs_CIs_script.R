419d1a2d272c792d09afd186ca92ef326e69ad99
Charlotte Malmborg <malmborg@scc-cc5.scc.bu.edu>
2025-08-01 11:28:22 -0400

    figuring out how to deal with the categorical variables (and then removing them from the plot later, lol)

--- 10_Recovery_State_Space_Outputs_CIs_script.R
+++ 10_Recovery_State_Space_Outputs_CIs_script.R
@@ -11,8 +11,8 @@ dir <- "/projectnb/dietzelab/malmborg/Ch2_PestRecovery/Recovery_State_Space_Runs
 setwd(dir)
 
 ## Load model output files
-model_params <- read.csv("2025_07_29_all_base_uni_recov_models_param_means.csv")
-load("2025_07_29_recov_models_outputs_list.RData")  # object is called model_outputs
+model_params <- read.csv("2025_07_31_all_base_uni_recov_models_param_means.csv")
+load("2025_07_31_recov_models_outputs_list.RData")  # object is called model_outputs
 
 ## Calculating CIs for all models
 # getting means, low CI (0.025 quantile), high CI (0.975 quantile):
@@ -50,13 +50,28 @@ model_results_CIs <- as.data.frame(model_means) %>%
   rename_with(~ paste0("low_", seq_along(.)), .cols = -c(1:6)) %>%
   # add the higher CI
   bind_cols(model_highs[,2:ncol(model_highs)]) %>%
-  rename_with(~ paste0("high_", seq_along(.)), .cols = -c(1:11)) #%>%
-  # # sort long
-  # pivot_longer(
-  #   cols = starts_with(c("mean", "high", "low")),
-  #   names_to = c("CI", "number"),
-  #   names_sep = c("_"),
-  #   values_to = "value")
+  rename_with(~ paste0("high_", seq_along(.)), .cols = -c(1:11))
+
+# fix the categorcial:
+model_results_CIs_cat <- model_results_CIs %>%
+  filter(.[[1]] == "uni_nlcd_cat") %>%
+  pivot_longer(
+    cols = matches("^(mean|low|high)_\\d+"),
+    names_to = c(".value", "set"),
+    names_pattern = "(mean|low|high)_(\\d+)") %>%
+  mutate(model = paste0("uni_nlcd_cat_", set), .before = 2) %>%
+  select(-set)
+
+# bind to original, fix column names:
+model_results_CIs <- model_results_CIs %>%
+  filter(!.[[1]] == "uni_nlcd_cat") %>%
+  mutate(mean = mean_1) %>%
+  mutate(low = low_1) %>%
+  mutate(high = high_1) %>%
+  select(model, mean, low, high) %>%
+  bind_rows(model_results_CIs_cat)
+#remove unnecessary things:
+rm(model_results_CIs_cat)
 
 
 ## Ridge Plot for betas
@@ -67,23 +82,29 @@ library(viridis)
 library(hrbrthemes)
 
 # get the data together:
-beta_ridges <- as.data.frame(matrix(NA, ncol = length(model_outputs)+4, nrow = nrow(model_outputs[[1]])))
-for (i in 1:length(model_outputs)){
+beta_list <- list()
+for (i in seq_along(model_outputs)) {
   model_name <- names(model_outputs)[i]
-  # get results from list:
   result <- model_outputs[[i]]
-  if (TRUE %in% grepl("beta", colnames(result))){
-    beta <- as.data.frame(result[,grep("beta", colnames(result))])
-    colnames(beta) <- "beta"
-    for(j in 1:ncol(beta)){
-      beta_ridges[,i] <- beta[j]
-      colnames(beta_ridges)[i] <- c(paste0("beta_", j, "_", model_name))
-    }
+  
+  # Find all columns with 'beta' in the name
+  beta_cols <- grep("beta", colnames(result), value = TRUE)
+  if (length(beta_cols) > 0) {
+    beta <- result[, beta_cols, drop = FALSE]  # keep as data frame
+    # Rename columns: beta_1_modelName, beta_2_modelName, etc.
+    new_names <- paste0("beta_", seq_along(beta_cols), "_", model_name)
+    colnames(beta) <- new_names
+    beta_list[[i]] <- beta
   } else {
-    colnames(beta_ridges)[i] <- c(paste0(model_name))
+    # No beta columns → create NA columns with proper row count
+    n_rows <- nrow(result)
+    na_col <- data.frame(matrix(NA, nrow = n_rows, ncol = 1))
+    colnames(na_col) <- model_name
+    beta_list[[i]] <- na_col
   }
 }
-rm(model_name, result, beta)
+# unlist and combine:
+beta_ridges <- do.call(cbind, beta_list)
 
 # pivot longer to prepare for making ridges:
 beta_ridges_long <- beta_ridges %>%
@@ -98,21 +119,25 @@ beta_ridges_long <- beta_ridges %>%
   mutate(model = fct_reorder(model, mean_beta, .desc = TRUE)) %>%
   # make model a factor for plotting:
   mutate(model = as.factor(model)) %>%
-  arrange(model)
+  arrange(model) %>%
+  # if we remove categorical...
+  filter(!str_detect(model, "nlcd"))
 
 # Now let's make the ridge plot:
-beta_ridge_plot <- ggplot(beta_ridges_long, aes(x = beta_est, y = model, fill = stat(x))) +
+beta_ridge_plot <- ggplot(beta_ridges_long, aes(x = beta_est, y = model, fill = after_stat(x))) +
   # title:
   labs(title = 'Slope Estimates') +
   # add color scaling:
-  geom_density_ridges_gradient(scale = 1.5) +
+  geom_density_ridges_gradient(scale = 5) +
   scale_fill_gradient2(
     low = "blue", mid = "white", high = "red", midpoint = 0) +
   # add vertical line at 0:
   geom_vline(xintercept = 0, linetype = "dashed", color = "black") +
+  # set limit x-axis:
+  xlim(c(-0.1, 0.125)) +
   theme_ipsum() +
   theme(legend.position = "none",
-        panel.spacing = unit(0.1, "lines"),
+        panel.spacing = unit(1, "lines"),
         strip.text.x = element_text(size = 8))
 
 beta_ridge_plot
