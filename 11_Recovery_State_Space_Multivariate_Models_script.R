c396af31dd867386d7cf7f7a8071a4446c5ca479
Charlotte Malmborg <malmborg@scc-kd2.scc.bu.edu>
2025-08-20 08:00:46 -0400

    first commmit - recovery state space multivariate models

---
+++ 11_Recovery_State_Space_Multivariate_Models_script.R
@@ -0,0 +1,15 @@
+### Multivariate Recovery State Space JAGS model script ###
+
+dir <- "/projectnb/dietzelab/malmborg/Ch2_PestRecovery/"
+setwd(dir)
+
+## Load libraries
+librarian::shelf(rjags, coda, dplyr, tidyverse)
+
+## Load data
+
+## Load models
+
+## Prepare data for models
+
+## Model runs
