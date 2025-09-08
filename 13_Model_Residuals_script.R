bb3867a1835509d6041939f4a66cd75508c87698
Charlotte Malmborg <malmborg@scc-kd1.scc.bu.edu>
2025-09-08 10:53:41 -0400

    setting up model residuals code for spatial and temporal autocorrelation

--- 13_Model_Residuals_script.R
+++ 13_Model_Residuals_script.R
@@ -9,5 +9,41 @@ library(rjags)
 dir <- "/projectnb/dietzelab/malmborg/Ch2_PestRecovery/Recovery_State_Space_Runs/"
 setwd(dir)
 
-## load best model
-models <- 
+## Load Best Model
+# all model files:
+models <- list.files(paste0(dir, "model_runs"))[grep("RData", list.files(paste0(dir, "model_runs")))]
+# best model:
+best <- 38  # also important: 42, 29, 7
+best_model <- models[best] 
+# load model:
+load(paste0(dir, "model_runs/", best_model))
+
+## Get Model Information and Data
+# get parameter values:
+best_model_params <- model_params[best,]
+# model name:
+model_name <- best_model
+if (grepl("cov", name) == TRUE){
+  model_name <- str_match(name, "cov_(.*?)_data")[,2]
+} else {
+  model_name <- str_match(name, "_(.*?)_model")[,2]
+}
+# load model inputs:
+model_inputs <- model_info$metadata$model_data
+
+## Extract Model Parameters:
+# model output:
+jags_out <- model_info$jags_out
+# model parameter names:
+jags_vars < varnames(jags_out)
+params <- jags_out[,grep("r0|^tau|^b|^at", vars)]
+# remove burn in:
+burn_in = 25000
+params_burn <- window(params, start = burn_in)
+# convert output to dataframe:
+params_out <- as.data.frame(as.matrix(params_burn))
+# separate out specific params:
+x_params <- grep("^x", colnames(out))
+beta_params <- grep("^b",colnames(out))
+taus <- grep("tau", colnames(out))
+r_int <- grep("r0", colnames(out))
