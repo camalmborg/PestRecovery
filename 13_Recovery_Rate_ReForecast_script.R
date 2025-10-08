a9de579c2991064c36a5d932d77fd52f55f877b2
Charlotte Malmborg <malmborg@scc-kd4.scc.bu.edu>
2025-10-08 13:01:57 -0400

    prepping model inputs from posterior for getting re-forecast running

--- 13_Recovery_Rate_ReForecast_script.R
+++ 13_Recovery_Rate_ReForecast_script.R
@@ -11,28 +11,87 @@ setwd(dir)
 
 # model files:
 models <- list.files(paste0(dir, "model_runs"))[grep("RData", list.files(paste0(dir, "model_runs")))]
-# get model:
-top_model <- dic_sort[dic_sort$perform == 1,]
-top_model <- top_model$model_number
-# load the best model:
-load(paste0(dir, "model_runs/", models[as.numeric(top_model)]))
+model_num = 1
 
-## Sample parameters from the model posterior
-# get parameters from model output:
-posterior <- as.matrix(model_info$jags_out)
-# random sample of an entire row:
-sample <- sample(1:nrow(posterior), 1)
-# make them the right objects to go into the forecast:
-# separate out specific params:
-beta_params <- grep("^b",colnames(posterior))
-taus <- grep("tau", colnames(posterior))
-r <- grep("r0", colnames(posterior))
-# group them:
-params <- cbind(posterior[,beta_params], posterior[,taus], posterior[,r])
+## Get model parameters for running forecast
+get_params <- function(model_num){
+  # get model:
+  top_model <- dic_sort[dic_sort$perform == model_num,]
+  top_model <- top_model$model_number
+  # load the best model:
+  load(paste0(dir, "model_runs/", models[as.numeric(top_model)]))
+  
+  ## Sample parameters from the model posterior
+  # get parameters from model output:
+  posterior <- as.matrix(model_info$jags_out)
+  # random sample of an entire row:
+  sample <- sample(1:nrow(posterior), 1)
+  # make them the right objects to go into the forecast:
+  # separate out specific params:
+  beta_params <- grep("^b",colnames(posterior))
+  taus <- grep("tau", colnames(posterior))
+  r <- grep("r0", colnames(posterior))
+  tau_time <- grep("at", colnames(posterior))
+  # group them:
+  params <- cbind(posterior[,beta_params], posterior[,taus], posterior[,tau_time], r = posterior[,r])
+  # sample:
+  params <- params[sample,]
+  return(params)
+}
+# model params:
+params <- get_params(model_num)
 
 ## Prepare the forecast
 # time steps:
 start = 2017
 end = 2023
 nt = 1:(end-start)
+# model covariates inputs:
+covs <- grep("cov", names(model_info$metadata$model_data))
+covariates <- do.call(cbind, model_info$metadata$model_data[covs])
+ncovs = length(covs)
+# initial conditions - 2017 tcg:
+ic <- model_info$metadata$model_data$x_ic
+
+## Forecast function
+#'@param nt = vector - timesteps
+#'@param n_ens = numeric - number of ensemble members
+#'@param ic = vector - initial condition, in this case 2017 TCG
+#'@param params = vector - parameters from get_params function
+#'@param covariates = covariates for model
+rss_forecast <- function(nt, n_ens, ic, params, covariates){
+  ## Prep params and inputs
+  # covariates:
+  cov_groups <- unique(sub("\\..*", "", colnames(covariates)))
+  cov_list <- list()
+  for (i in cov_groups){
+    cov_list[[i]] <- covariates[,grep(i, colnames(covariates))]
+  }
+  # assign:
+  for (name in names(cov_list)) {
+    assign(name, cov_list[[name]])
+  }
+  # model parameters:
+  # betas:
+  betas <- params[grep("beta", names(params))]
+  # taus - convert to SD from 1/precision:
+  tau_obs <- 1/params[grep("obs", names(params))]
+  tau_add <- 1/params[grep("add", names(params))]
+  tau_time <- 1/params[grep("atime", names(params))]
+  tau_time[is.infinite(tau_time)] <- 0  # fix Inf value from conversion to SD from precisions
+  # r0:
+  r <- params[grep("r", names(params))]
+  
+  ## Run the forecast
+  # matrix to hold results:
+  N <- matrix(NA, nrow = n_ens, ncol = last(nt))
+  # loop over time:
+  for (t in 1:nt){
+    R[s] <- r + tau_time[t] + (betas[1]*cov_list$cov_one[s,t]) + (betas[2]*cov_two[s,t]) + betas[3]*cov_three[s]
+    mu[s,t] <- R[s] * x[s,t-1]  
+    # x[s,t] <- dnorm(mu[s,t], tau_add)
+    # x[s,1] <- ic[s]
+    }
+}
+
 
