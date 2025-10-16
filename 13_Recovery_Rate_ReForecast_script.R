ab7b52b20bcd0173875536104fe7cd8664142612
Charlotte Malmborg <malmborg@scc-ga1.scc.bu.edu>
2025-10-16 12:39:27 -0400

    changed get_params function to get same parameters for each year of the analysis - still have to add thing about starting from proper year for 2018-onward

--- 13_Recovery_Rate_ReForecast_script.R
+++ 13_Recovery_Rate_ReForecast_script.R
@@ -19,26 +19,31 @@ top_model <- top_model$model_number
 load(paste0(dir, "model_runs/", models[as.numeric(top_model)]))
 
 ## Get model parameters for running forecast
-get_params <- function(model_info, yr){
+#'@param model_info = model output that loads from folders including model posterior and input data saved in model runs
+#'@param n_ens = number of ensemble members for running re-forecast
+get_params <- function(model_info, n_ens){
   ## Sample parameters from the model posterior
   # get parameters from model output:
   posterior <- as.matrix(model_info$jags_out)
   # random sample of an entire row:
-  sample <- sample(1:nrow(posterior), 1)
+  sample <- sample(1:nrow(posterior), n_ens, replace = FALSE)
   # make them the right objects to go into the forecast:
   # separate out specific params:
   beta_params <- grep("^b",colnames(posterior))
   taus <- grep("tau", colnames(posterior))
   r <- grep("r0", colnames(posterior))
   tau_time <- grep("at", colnames(posterior))
-  x_ic <- grep(paste0("^x\\[[0-9+,", as.character(yr),"\\]"), colnames(posterior))
+  x_ic <- grep(paste0("^x\\[[0-9+,1\\]"), colnames(posterior))
   # group them:
   params <- cbind(posterior[,beta_params], posterior[,taus], posterior[,tau_time], r = posterior[,r], posterior[,x_ic])
   # sample:
-  params <- params[sample,]
+  params <- cbind(col_num = sample, params[sample,])
   return(params)
 }
-
+# number of ensemble members:
+n_ens = 5
+# get sampled parameters: nrow = n_ens
+ens_params <- get_params(model_info, n_ens)
 
 ## Prepare the forecast
 # model covariates inputs:
@@ -57,21 +62,17 @@ for (name in names(cov_list)) {
 }
 
 # time steps:
-yr = 2
-start = 2018
+yr = 1
+start = 2017
 end = 2023
 nt = 1:(end-start)
-# number of ensemble members:
-n_ens = 2
 # number of sites:
 ns = 2
-# # matrix to hold results:
-# N <- matrix(NA, nrow = n_ens, ncol = length(nt)+1)
 
 
 ## Run the forecast
 # function to run across all sites and ensemble members:
-run_forecast <- function(nt, ns, n_ens){
+run_forecast <- function(nt, ns, n_ens, params){
   # empty list to hold result:
   forecast_result <- list()
   # loop over sites:
@@ -80,32 +81,29 @@ run_forecast <- function(nt, ns, n_ens){
     N <- matrix(NA, nrow = n_ens, ncol = length(nt)+1)
     # loop over ensemble members:
     for (i in 1:n_ens){
-      # model params:
-      params <- get_params(model_info, yr)
-      
       ## Prep params and inputs
       # model parameters:
       # betas:
-      betas <- params[grep("beta", names(params))]
+      betas <- params[n_ens, c(grep("beta", colnames(params)))]
       # taus - convert to SD from 1/precision:
-      tau_obs <- 1/params[grep("obs", names(params))]
-      tau_add <- 1/params[grep("add", names(params))]
-      a_time <- c(0, 1/params[grep("atime", names(params))])
+      tau_obs <- 1/params[n_ens, c(grep("obs", colnames(params)))]
+      tau_add <- 1/params[n_ens, c(grep("add", colnames(params)))]
+      a_time <- c(0, 1/params[n_ens, c(grep("atime", colnames(params)))])
       a_time[is.infinite(a_time)] <- 0  # fix Inf value from conversion to SD from precisions
       # r0:
-      r <- params[grep("r", names(params))]
+      r <- params[n_ens, c(grep("r", colnames(params)))]
       
       ## Run the forecast
       c_one = cov_one[s,]
       c_two = cov_two[s,]
       c_three = cov_three[s]
       # x_ic:
-      N[,1] <- params[grep("^x", names(params))][s]
+      N[,1] <- params[n_ens, c(grep("^x", colnames(params)))][s]
       
       # loop over time:
       for (t in 2:ncol(N)){
-        R <- r + a_time[t-1] + (betas[1]*c_one[,t-1]) + (betas[2]*c_two[,t-1]) + betas[3]*c_three
-        N[i,t] <- rnorm(1, mean = N[i,t-1]*R, sd = tau_add)
+        big_R <- r + a_time[t-1] + (betas[1]*c_one[,t-1]) + (betas[2]*c_two[,t-1]) + betas[3]*c_three
+        N[i,t] <- rnorm(1, mean = N[i,t-1]*big_R, sd = tau_add)
       }
     }
     forecast_result[[s]] <- as.data.frame(N)
@@ -116,4 +114,4 @@ run_forecast <- function(nt, ns, n_ens){
   return(forecast_result)
 }
 
-test <- run_forecast(nt = nt, ns = ns, n_ens = n_ens)
+test <- run_forecast(nt = nt, ns = ns, n_ens = n_ens, params = ens_params)
