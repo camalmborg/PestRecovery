d7227f493cc4892abf03e8ffd3a77ca5a7fa283e
Charlotte Malmborg <malmborg@scc-be8.scc.bu.edu>
2025-10-22 12:28:47 -0400

    made CRPS score loop for all models, will have to set up and run after re-running forecasts code with all bugs fixed

--- 13_ReForecast_Results_script.R
+++ 13_ReForecast_Results_script.R
@@ -13,22 +13,50 @@ setwd(dir)
 years <- 2017:2023
 tcg <- read.csv("/projectnb/dietzelab/malmborg/Ch2_PestRecovery/Data/tcg_5ksamp_clean.csv")[-1] %>%
   # select columns with observations for 2017-2023:
-  select(matches(as.character(years))) 
+  select(matches(as.character(years))) %>%
+  # rename columns for years:
+  rename_with(~ as.character(years)[seq_along(.)])
   
 # load model forecast files:
 files <- list.files(pattern = "result.csv$")
-#forecast_list <- lapply(files, read.delim)
-
+model_num = as.numeric(Sys.getenv("SGE_TASK_ID"))
 # loading test file:
-test <- read.csv(files[1])
-
-testing <- test %>%
+model_out <- read.csv(files[model_num])
+pred <- model_out %>%
   # rename columns with years:
-  rename_with(~ as.character(years)[seq_along(.)], .cols = -1) %>%
-  # remove 2017 to get only the predicted values:
-  select(-"2017")
+  rename_with(~ as.character(years)[seq_along(.)], .cols = -1)
 
-  
+## Get CRPS scores for ensemble model
+# make matrix to hold scores:
+crps_scores <- matrix(nrow = nrow(tcg), ncol = ncol(tcg))
+colnames(crps_scores) <- as.character(years)
+site = 1:nrow(tcg)
+# run scores for every site/year:
+for (s in site){
+  for (i in years){
+    # get observation:
+    obs <- tcg[s, as.character(i)]
+    # get ensemble for each site:
+    ens <- pred[pred$site == s, as.character(i)]
+    # if NA:0
+    if (is.na(obs)|any(is.na(ens))){
+      crps_scores[s, as.character(i)] <- NA
+    } else {
+    # calculate crps:
+    crps <- scoringRules::crps_sample(y = obs, dat = ens)
+    # fill in table:
+    crps_scores[s, as.character(i)] <- crps
+    }
+  }
+}
+
+
+# # get observed value for each site:
+# year = years[1]
+# site = 1
+# obs <- tcg[site, as.character(year)]
+# ens <- testing[testing$site == 1, as.character(year)]
+# crps <- scoringRules::crps_sample(y = obs, dat = ens)
   
 # testing <- test %>%
 #   group_by(site) %>% 
