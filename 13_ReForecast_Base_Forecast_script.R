e65392246b3697aee62b7cfcf4a19e485e167944
Charlotte Malmborg <malmborg@scc-kd2.scc.bu.edu>
2025-12-22 05:15:09 -0500

    prepped base forecast for runs

--- 13_ReForecast_Base_Forecast_script.R
+++ 13_ReForecast_Base_Forecast_script.R
@@ -12,19 +12,19 @@ setwd(dir)
 # load environment if needed:
 load("/projectnb/dietzelab/malmborg/Ch2_PestRecovery/Environments/2025_07_07_environment.RData")
 
-## Input data:
-# first x:
-x_miss <- mean(tcg[,grep("^2017",names(tcg))], na.rm = T)
-x_prec_miss <- sd(tcg[,grep("^2017", names(tcg))], na.rm = T)
-x1 <- tcg[,grep("2017", names(tcg))]
-x1[which(is.na(x1))] <- x_miss
-# x1 precision:
-x_prec <- 1/(sd_tcg[,grep("2017", names(sd_tcg))]^2)
-x_prec[which(is.na(x_prec))] <- 1/x_prec_miss
-x_prec[which(x_prec == Inf)] <- 1/x_prec_miss
+## Input data
+# get mean tcg decay baseline 2017 data:
+tcg_base <- tcg_ts %>%
+  # pre-disturbance 5 year baseline:
+  mutate(baseline = rowMeans(select(., `2010-05-01`:`2015-05-01`), na.rm = TRUE), .before = 1) %>%
+  # pre-disturbance 5 year sd:
+  mutate(sd = apply(pick(`2010-05-01`:`2015-05-01`), 1, sd, na.rm = TRUE), .before = 2)
+# extract them as vectors:
+means <- tcg_base$baseline
+sds <- tcg_base$sd
 
-# forecast for base (suggests next year is just mean of last year plus some sd):
-run_base_forecast <- function(start, end, ns, n_ens, xs, xprec, yr){
+# forecast for base (suggests next year is just mean of last year plus some sd as prec):
+run_base_forecast <- function(start, end, ns, n_ens, means, sds, yr){
   # time steps:
   nt = 1:(end-start)
   # empty list to hold result:
@@ -35,16 +35,9 @@ run_base_forecast <- function(start, end, ns, n_ens, xs, xprec, yr){
     N <- matrix(NA, nrow = n_ens, ncol = length(nt)+1)
     # loop over ensemble members:
     for (i in 1:n_ens){
-      # x_ic:
-      x_ic <- xs[s]
-      N[,1] <- x_ic
-      
-      # tau_add:
-      tau_add <- sqrt(1/xprec[s,])
-      
       # loop over time:
-      for (t in 2:ncol(N)){
-        N[i,t] <- rnorm(1, mean = N[i,t-1], sd = tau_add[s,t])
+      for (t in 1:ncol(N)){
+        N[i,t] <- N[i,t] <- rnorm(1, mean = means[s], sd = sds[s])
       }
     }
     forecast_result[[s]] <- as.data.frame(N)
@@ -57,18 +50,32 @@ run_base_forecast <- function(start, end, ns, n_ens, xs, xprec, yr){
 
 
 ## Running for different models
+# data.frame of model jobs:
+model_jobs <- data.frame(task_id = 1:18, 
+                         year_run = rep(c(1:6), 3))
 # preparing inputs:
 # number of sites:
 ns = 5000
 # number of ensemble members:
 n_ens = 1500
-
-# get sampled parameters: 
-ens_params <- model_params[[model_num]]  # for these runs, using pre-sampled to make sure all time horizons have the same parameters for each model
-#ens_params <- read.csv(paste0("Recovery_Forecasts/model_", as.character(model_num), "_params.csv"))
-#ens_params <- get_params(model_info, n_ens)
 # years of analysis:
 years <- 2017:2023
 n_yr <- 1:(length(years)-1)
 # use taskid to select proper years for model run:
 y = model_jobs$year_run[task_id]
+
+## Run base forecast
+base_forecast <- run_base_forecast(start = years[y], 
+                                   end = last(years), 
+                                   ns = ns, 
+                                   n_ens = n_ens, 
+                                   means = means,
+                                   sds = sds,
+                                   yr = y)
+write.csv(base_forecast, file = paste0("Recovery_Forecasts/", Sys.Date(),
+                                    "_ens_", as.character(n_ens),
+                                    "_base_model_", 
+                                    "_start_year_", as.character(years[y]),
+                                    "_reforecast_result.csv"),
+          row.names = FALSE)
+
