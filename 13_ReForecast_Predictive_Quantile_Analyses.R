62c2656b3213eea4539cb68a9ad48c161c66090e
Charlotte Malmborg <malmborg@scc-cc6.scc.bu.edu>
2026-03-16 11:22:39 -0400

    ready to run predictive quantiles for each forecast

--- 13_ReForecast_Predictive_Quantile_Analyses.R
+++ 13_ReForecast_Predictive_Quantile_Analyses.R
@@ -1,5 +1,8 @@
 ### Predictive Quantiles analysis
 
+# load libraries
+librarian::shelf(dplyr, tidyverse, stringr, ggplot2)
+
 ## Getting TCG observations
 # get baselines:
 tcg_base <- read.csv("/projectnb/dietzelab/malmborg/Ch2_PestRecovery/Data/tcg_5ksamp_clean.csv")[-1] %>%
@@ -21,42 +24,83 @@ tcg <- read.csv("/projectnb/dietzelab/malmborg/Ch2_PestRecovery/Data/tcg_5ksamp_
   # rename with just years:
   rename_with(~ sub("-.*", "", .x))
 
+
+## Comparing forecasts with observations:
+# set wd:
+dir <- "/projectnb/dietzelab/malmborg/Ch2_PestRecovery/Recovery_State_Space_Runs/"
+setwd(dir)
+
+## Load forecast
+forecasts <- grep("^2025", list.files("Recovery_Forecasts/"))
+# setting task id for running array job on SCC:
+task_id <- as.numeric(Sys.getenv("SGE_TASK_ID"))
+# loading forecast:
+file <- list.files("Recovery_Forecasts/")[forecasts[task_id]]
+# year start:
+year <- as.numeric(str_extract(file, "(?<=year_)\\d+(?=_)"))
+fyears <- year:2023
 # load forecast:
-f <- read.csv("Recovery_Forecasts/2025-12-03_ens_1500_model_1_start_year_2017_reforecast_result.csv")
-fcast <- f %>%
-# add baseline to predictions:
-mutate(across(-site, ~ tcg_base$baseline[site] - .x))
+f <- read.csv(paste0("Recovery_Forecasts/", file))
+# add back baseline to get TCG predictions:
+fcast <- f |>
+  # add baseline to predictions:
+  mutate(across(-site, ~ tcg_base$baseline[site] - .x))
+# rename columns:
+colnames(fcast) <- c("site", as.character(fyears))
+
+# match tcg years:
+tcg_f <- tcg[,c(names(fcast)[-1])]
+
+## Get predictive quantiles
+# collecting quantiles:
+qsite <- matrix(data = NA, nrow = nrow(tcg_f), ncol = ncol(tcg_f))
+## Compare each ensemble member for each site
+for (i in 1:nrow(tcg_f)){
+  for (j in 1:ncol(tcg_f)){
+    fsite <- fcast[which(fcast$site == i), -1][,j]
+    qsite[i,j] <-  sum(fsite < tcg_f[i,j])/1500 
+  }
+}
 
 
-
-
-# where forecast > tcg values:
-tf_df <- as.data.frame((y_pred[, -1] > tcg) * 1)
-colnames(tf_df) <- c(as.character(2017:2023))
-tf_sums <- colSums(tf_df, na.rm = T)
-
-# save it:
-save_dir <- "/projectnb/dietzelab/malmborg/Ch2_PestRecovery/Figures/"
-setwd(save_dir)
+## Saving result
+# set save directory:
+save_dir <- "Recovery_Forecasts/Predictive_Quantiles/"
 # save:
-png(filename = paste0(save_dir, "2026_03_15_predictive_histogram_forecast_vs_tcg.png"),
-    height = 6,
-    width = 8,
-    units = "in",
-    res = 600)
+file_name <- gsub(".csv", "", file)
+write.csv(qsite, file = paste0(save_dir, file_name, "_pred_quant.csv"))
 
-tf_df |>
-  pivot_longer(everything(), names_to = "column", values_to = "value") |>
-  count(column, value) |>
-  filter(!is.na(value)) |>
-  ggplot(aes(x = column, y = n, fill = factor(value, levels = c(0,1),
-                                              labels = c("Underestimated","Overestimated")))) +
-  scale_fill_manual(values = c("Underestimated" = "skyblue1", "Overestimated" = "indianred1")) +
-  geom_col(position = position_dodge(width = 0.6), width = 0.55) +
-  labs(x = "Year", y = "Count", fill = "Value") +
-  theme_bw() +
-  theme(axis.text = element_text(size = 14),      
-        axis.title = element_text(size = 16), 
-        legend.text = element_text(size = 14),
-        legend.title = element_blank())
-dev.off()
+
+
+
+
+# # where forecast > tcg values:
+# tf_df <- as.data.frame((y_pred[, -1] > tcg) * 1)
+# colnames(tf_df) <- c(as.character(2017:2023))
+# tf_sums <- colSums(tf_df, na.rm = T)
+# 
+# # save it:
+# save_dir <- "/projectnb/dietzelab/malmborg/Ch2_PestRecovery/Figures/"
+# setwd(save_dir)
+# # save:
+# png(filename = paste0(save_dir, "2026_03_15_predictive_histogram_forecast_vs_tcg.png"),
+#     height = 6,
+#     width = 8,
+#     units = "in",
+#     res = 600)
+# 
+# tf_df |>
+#   pivot_longer(everything(), names_to = "column", values_to = "value") |>
+#   count(column, value) |>
+#   filter(!is.na(value)) |>
+#   ggplot(aes(x = column, y = n, fill = factor(value, levels = c(0,1),
+#                                               labels = c("Underestimated","Overestimated")))) +
+#   scale_fill_manual(values = c("Underestimated" = "skyblue1", "Overestimated" = "indianred1")) +
+#   geom_col(position = position_dodge(width = 0.6), width = 0.55) +
+#   labs(x = "Year", y = "Count", fill = "Value") +
+#   theme_bw() +
+#   theme(axis.text = element_text(size = 14),      
+#         axis.title = element_text(size = 16), 
+#         legend.text = element_text(size = 14),
+#         legend.title = element_blank())
+# dev.off()
